d56b4d5f86b0736100728324a9fec9c88bf3058f
jardmclan <mcleanj@hawaii.edu>
2024-03-13 17:17:29 -1000

    fix default date

--- containers/daily/task.sh
+++ containers/daily/task.sh
@@ -8,12 +8,10 @@ if [ $AGGREGATION_DATE ]; then
     echo "An aggregation date was provided by the environment."
     echo "Aggregation date is: " $AGGREGATION_DATE
 else
-    export AGGREGATION_DATE=$(date --iso-8601)
-    echo "No aggregation date was provided by the environment. Defaulting to today."
+    export AGGREGATION_DATE=$(date -d "1 day ago" --iso-8601)
+    echo "No aggregation date was provided by the environment. Defaulting to yesterday."
     echo "Aggregation date is: " $AGGREGATION_DATE
 fi
-export AGGREGATION_DATE_YESTERDAY=$(date --date="$AGGREGATION_DATE - 1 day" --iso-8601)
-echo "Yesterday is: " $AGGREGATION_DATE_YESTERDAY
 
 echo "[task.sh] [2/7] Fetching acquisition data and dependencies."
 #"Fetching acquisition data and dependencies"
